8f1565f361a958594c0aa14567c9413b1f945848
trimendos <trimendos@gmail.com>
2018-10-25 14:21:28 +0300

    Updates

---
+++ precache-manifest.a640f8c3b63dfc3610813b55e4fc8c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crawler-budget-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90c021c3baebf233547b",
+    "url": "/crawler-budget-react/static/js/runtime~main.90c021c3.js"
+  },
+  {
+    "revision": "d540f4217d9aa7d0fca0",
+    "url": "/crawler-budget-react/static/js/main.d540f421.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/crawler-budget-react/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "d540f4217d9aa7d0fca0",
+    "url": "/crawler-budget-react/static/css/main.e3818aee.chunk.css"
+  },
+  {
+    "revision": "5e24ea1bf89ad7d8ad1850f4b0d9a41c",
+    "url": "/crawler-budget-react/index.html"
+  }
+];
